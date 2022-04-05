6f6e16168050eb4922301c9a2abc0dbef0b2b45b
Housseem-Kouki <houssem.kouki@esprit.tn>
2022-04-05 11:13:31 +0200

    first

---
+++ src/adomifitregime/AdomifitRegime.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package adomifitregime;
+
+import entites.Calendar;
+import entites.CategorieRegime;
+import entites.Regime;
+import entites.SuiviRegime;
+import services.CalendarService;
+import services.CategorieRegimeService;
+import services.RegimeService;
+import services.SuiviRegimeService;
+import utils.DataSource;
+import java.text.SimpleDateFormat;  
+import java.time.LocalDate;
+import java.util.Date; 
+/**
+ *
+ * @author Kouki
+ */
+public class AdomifitRegime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CategorieRegimeService serv = new CategorieRegimeService();
+         RegimeService servReg = new RegimeService();
+         SuiviRegimeService servSuiv = new SuiviRegimeService();
+         CalendarService servCalendar = new CalendarService();
+        // Categorie regime crud 
+        //affichage All
+//       CategorieRegimeService serv = new CategorieRegimeService();
+//       serv.getAll().forEach(System.out::println);
+       
+       
+       
+        // insertion categorie
+        //stat color length in db = 7
+//        CategorieRegime cr = new CategorieRegime("desktopTest","desktopTest","desktop");
+//        serv.addCategorieRegime(cr);
+        
+        // delete categorie
+//        serv.deleteCategorieRegime(76);
+
+        //get one categorie
+//        CategorieRegime cr = new CategorieRegime();   
+//        cr = serv.getCategorieRegime(9);
+//        System.out.println(cr.toString());
+
+        //update Categorie    
+//        CategorieRegime cr = new CategorieRegime(16,"salut","up","up");
+//        serv.update(cr);
+        
+       
+
+
+
+
+
+
+
+
+
+             //affichage All Regimes
+//        RegimeService servReg = new RegimeService();
+//       servReg.getAll().forEach(System.out::println);
+
+        // insertion regime
+//      Regime r = new Regime("salut desk", "salut desk", "salut desk", "salut desk", 120, 9, 2);
+//       servReg.addRegime(r);
+
+        // delete regime
+//        servReg.deleteRegime(193);
+
+
+        //get one regime
+//        Regime r = new Regime();   
+//        r = servReg.getRegime(25);
+//        System.out.println(r.toString());
+
+        //update regime    
+//       Regime r = new Regime(194, "updated", "updated", "updated", "updated", 167, 9, 2);
+//        servReg.updateRegime(r);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             //affichage All suivis
+ //              servSuiv.getAll().forEach(System.out::println);
+
+         // insertion suivi
+//         SuiviRegime sr = new SuiviRegime("new desktop", "new desktop", 16, 24, 1);
+//         servSuiv.addSuiviRegime(sr);
+
+                    // delete suivi
+              //   servSuiv.deleteSuiviRegime(45);
+              
+              
+                      //get one suivi
+//            SuiviRegime sr = new SuiviRegime();
+//            sr = servSuiv.getSuiviRegime(46);
+//            System.out.println(sr.toString());
+
+        //update suivi
+//        SuiviRegime sr = new SuiviRegime(46, "updated", "updated", 2, 24, 1);
+//        servSuiv.update(sr);
+
+
+
+
+
+              
+              
+             //affichage All calendar
+//             servCalendar.getAll().forEach(System.out::println); 
+
+         // insertion calendar LocalDate.parse("2018-05-05")
+       //  Calendar c = new Calendar("new",new Date("2018-05-05") , LocalDate.parse("2018-05-05"), "new calendar", true, "new cal", "new cal", "new cal", 19, true);
+//         SuiviRegime sr = new SuiviRegime("new desktop", "new desktop", 16, 24, 1);
+//         servSuiv.addSuiviRegime(sr);
+
+ 
+        
+    }
+    
+}
---
+++ src/entites/Regime.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entites;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ *
+ * @author Kouki
+ */
+public class Regime {
+        
+    private int id; 
+    private String type; 
+    private String description; 
+    private String image; 
+    private String dificulte; 
+    private float prix;
+    
+    private int categorie_regime_id ;
+    private int user_id;
+    private LocalDateTime created_at;
+    
+      
+        
+    public Regime() {
+    }
+
+    public Regime(int id, String type, String description, String image, String dificulte, float prix, int categorie_regime_id, int user_id) {
+        this.id = id;
+        this.type = type;
+        this.description = description;
+        this.image = image;
+        this.dificulte = dificulte;
+        this.prix = prix;
+        this.categorie_regime_id = categorie_regime_id;
+        this.user_id = user_id;
+    }
+
+    public Regime(String type, String description, String image, String dificulte, float prix, int categorie_regime_id, int user_id) {
+        this.type = type;
+        this.description = description;
+        this.image = image;
+        this.dificulte = dificulte;
+        this.prix = prix;
+        this.categorie_regime_id = categorie_regime_id;
+        this.user_id = user_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDificulte() {
+        return dificulte;
+    }
+
+    public void setDificulte(String dificulte) {
+        this.dificulte = dificulte;
+    }
+
+    public float getPrix() {
+        return prix;
+    }
+
+    public void setPrix(float prix) {
+        this.prix = prix;
+    }
+
+    public int getCategorie_regime_id() {
+        return categorie_regime_id;
+    }
+
+    public void setCategorie_regime_id(int categorie_regime_id) {
+        this.categorie_regime_id = categorie_regime_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+     
+    @Override
+    public String toString() {
+        return "Regime{" + "id=" + id + ", type=" + type + ", description=" + description + ", image=" + image + ", dificulte=" + dificulte + ", prix=" + prix + ", categorie_regime_id=" + categorie_regime_id + ", user_id=" + user_id + '}';
+    }
+    
+   
+}
